d842d4f1e20d83630652a5ec0f116ca7f34ab416
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-01-29 16:50:50 +0800

    installer works yipee

--- src/installer/installer.py
+++ src/installer/installer.py
@@ -1,14 +1,21 @@
 from subprocess import run
 from sys import executable as pyexe
+import os
+if str(os.getcwd()).endswith("system32"):
+		# This has to be in every script to prevent FileNotFoundError
+		# Because for some reason, it runs it at C:\Windows\System32
+		# Yeah, it is stupid, but I can't put these lines in custom_funct>
+		# Because that still brings up an error
+		os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 def require(module, module_name=""):
-    try:
-        __import__(module)
-    except ImportError:
-        if module_name == "":
-            run([pyexe, "-m", "pip", "install", module, "--quiet"])
-        else:
-            run([pyexe, "-m", "pip", "install", module_name, "--quiet"])
+		try:
+				__import__(module)
+		except ImportError:
+				if module_name == "":
+						run([pyexe, "-m", "pip", "install", module, "--quiet"])
+				else:
+						run([pyexe, "-m", "pip", "install", module_name, "--quiet"])
 
 require("colorama")
 from colorama import *
@@ -16,25 +23,46 @@ require("requests")
 import requests
 
 def print_tag(tag, message):
-    if tag == "error":
-        print(f"{Back.RED} ERROR {Back.RESET}\t{message}")
-    elif tag == "watch":
-        print(f"{Back.BLUE} WATCH {Back.RESET}\t{message}")
-    elif tag == "dev":
-        print(f"{Back.LIGHTBLACK_EX} DEV {Back.RESET}\t{message}")
-    elif tag == "warn":
-        print(f"{Back.YELLOW} WARN {Back.RESET}\t{message}")
-    elif tag == "info":
-        print(f"{Back.CYAN} INFO {Back.RESET}\t{message}")
+		if tag == "error":
+				print(f"{Back.RED} ERROR {Back.RESET}\t{message}")
+		elif tag == "watch":
+				print(f"{Back.BLUE} WATCH {Back.RESET}\t{message}")
+		elif tag == "dev":
+				print(f"{Back.LIGHTBLACK_EX} DEV {Back.RESET}\t{message}")
+		elif tag == "warn":
+				print(f"{Back.YELLOW} WARN {Back.RESET}\t{message}")
+		elif tag == "info":
+			  print(f"{Back.CYAN} INFO {Back.RESET}\t{message}")
+		else:
+				print_tag("error", f"Function `print_tag` does not support tag = {tag}")
 
 # change branch name
 remote_url = "https://raw.githubusercontent.com/BEComTweaks/boulder/refs/heads/im-cooking-please-wait/src/"
-response = requests.get(f'{remote_url}/installer/files.json')
-if response.status_code == 200:
-    print_tag("info", "Fetched files to install.")
-    files = response.json()
-    
-else:
-    print_tag("error", "Failed to fetch files to install.")
-    print_tag("error", f"Status code: {response.status_code}")
-    exit(1)
+try:
+	response = requests.get(f'{remote_url}/installer/files.json')
+	if response.status_code == 200:
+		print_tag("info", "Fetched files to install.")
+		files = response.json()
+		for script in files["python"]:
+			response = requests.get(f'{remote_url}/{script}')
+			if response.status_code == 200:
+				with open(script, "w") as file:
+					file.write(response.text)
+			else:
+				raise requests.exceptions.ConnectionError
+		for other_file in files["others"]:
+			response = requests.get(f'{remote_url}/{other_file}')
+			if response.status_code == 200:
+				with open(other_file, "w") as file:
+					file.write(response.text)
+			else:
+				raise requests.exceptions.ConnectionError
+	else:
+		raise requests.exceptions.ConnectionError
+except requests.exceptions.ConnectionError:
+	print_tag("error", "Failed to fetch files to install.")
+	try:
+		print_tag("error", f"Status code: {response.status_code}")
+	except NameError:
+		print_tag("error", "Please connect to an active internet connection.")
+	exit(1)
