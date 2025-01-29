fd744ae02f8c37e10402330cfe7225c45ea994c8
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-01-29 15:33:20 +0800

    working on installer

--- src/installer/installer.py
+++ src/installer/installer.py
@@ -24,3 +24,17 @@ def print_tag(tag, message):
         print(f"{Back.LIGHTBLACK_EX} DEV {Back.RESET}\t{message}")
     elif tag == "warn":
         print(f"{Back.YELLOW} WARN {Back.RESET}\t{message}")
+    elif tag == "info":
+        print(f"{Back.CYAN} INFO {Back.RESET}\t{message}")
+
+# change branch name
+remote_url = "https://raw.githubusercontent.com/BEComTweaks/boulder/refs/heads/im-cooking-please-wait/src/"
+response = requests.get(f'remote_url/installer/files.json')
+if response.status_code == 200:
+    print_tag("info", "Fetched files to install.")
+    files = response.json()
+    
+else:
+    print_tag("error", "Failed to fetch files to install.")
+    print_tag("error", f"Status code: {response.status_code}")
+    exit(1)
