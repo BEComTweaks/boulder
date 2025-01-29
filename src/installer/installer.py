8b88b858ed2d1f1d1273006318184f4b0d87a42e
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-01-30 02:22:42 +0800

    cooked the installer (havent tried windows)
    average experience of coding at 02:22am in the morning on my phone

--- src/installer/installer.py
+++ src/installer/installer.py
@@ -4,7 +4,7 @@ import os
 if str(os.getcwd()).endswith("system32"):
 		# This has to be in every script to prevent FileNotFoundError
 		# Because for some reason, it runs it at C:\Windows\System32
-		# Yeah, it is stupid, but I can't put these lines in custom_funct>
+		# Yeah, it is stupid, but I can't put these lines in custom_functons
 		# Because that still brings up an error
 		os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
@@ -21,6 +21,17 @@ require("colorama")
 from colorama import *
 require("requests")
 import requests
+import os
+import sys
+
+if os.name == "nt":
+		if "HOME" in os.environ and "USERPROFILE" in os.environ:
+				terminal_type = "unix"
+		else:
+				terminal_type = "cmd"
+				import winreg
+else: 
+		terminal_type = "unix"
 
 def print_tag(tag, message):
 		if tag == "error":
@@ -57,6 +68,31 @@ try:
 					file.write(response.text)
 			else:
 				raise requests.exceptions.ConnectionError
+		print_tag("info", "Pulled all files")
+		new_path = os.path.dirname(__file__)
+		if terminal_type == "cmd":
+			with winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Environment", 0, winreg.KEY_ALL_ACCESS) as key:
+				current_path = winreg.QueryValueEx(key, "Path")[0]
+				updated_path = current_path + ";" + new_path
+				winreg.SetValueEx(key, "Path", 0, winreg.REG_EXPAND_SZ, updated_path)
+			with open("boulder.bat", "w") as script:
+				script.write('@echo off\npython "%~dp0boulder.py" %*')
+		else:
+			print_tag("info", "I need more info on your terminal installation!")
+			print_tag("info", "Please input the terminal's type (`bash`, `zsh`, `fish`, etc)")
+			terminal_type = input()
+			shell_rc = os.path.join(os.environ["HOME"], f".{terminal_type}rc")
+			if os.path.exists(shell_rc):
+				with open(shell_rc, "a") as file:
+					file.write(f"export PATH=$PATH:{new_path}\n")
+			else:
+				print_tag("error", "Give the Terminal's actual name")
+				exit(1)
+			with open("boulder", "w") as script:
+				script.write('#!/bin/bash\npython "$(dirname "$0")/boulder.py" "$@"')
+			os.system("chmod +x boulder")
+		os.remove(__file__)
+		print_tag("info", "Install successful!")
 	else:
 		raise requests.exceptions.ConnectionError
 except requests.exceptions.ConnectionError:
