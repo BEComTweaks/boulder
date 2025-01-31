9d8bf46d90fa04eead1994005af20c8fc64fd5ee
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-01-31 15:10:31 +0800

    i hate this already
    some more function fixes
    
    i will make my own argparser because the actual argparser doesnt support
    flags without a hyphen ("-")

--- src/boulder.py
+++ src/boulder.py
@@ -1,15 +1,45 @@
 from custom_functions import *
+
 console = Console()
 
 require("colorama")
 from colorama import *
 import argparse
 
-args = argparse.ArgumentParser(description=f"Boulder\n{Fore.CYAN}Regolith, built with Python{Fore.RESET}")
-args.add_argument("-d", "--dev", help="Builds and moves the project automatically to the development folder")
-args.add_argument("-w", "--watch", help="Watches the project for changes and builds it automatically")
+args = argparse.ArgumentParser(
+    description=f"Boulder\n{Fore.CYAN}Regolith, built with Python{Fore.RESET}"
+)
+args.add_argument(
+    "-b",
+    "--build",
+    help=f"Builds the project to your other folder.{Fore.RED}This is automatically enabled when running --watch and/or --dev",
+)
+args.add_argument(
+    "-d",
+    "--dev",
+    help="Builds and moves the project automatically to the development folder",
+    action="store_true",
+)
+args.add_argument(
+    "-w",
+    "--watch",
+    help="Watches the project for changes and builds it automatically",
+    action="store_true",
+)
 args.add_argument("-v", "--verbose", help="Enable verbose mode", action="store_true")
+args.add_argument(
+    "-i", "--init", help="Initialize a Boulder project", action="store_true"
+)
 args = args.parse_args()
 
 boulder_config = load_boulder_config()
-
+if boulder_config == None and not args.init:
+    console.error("Could not find boulder_config.json")
+    console.tip("Did you mean to initialize?")
+    exit(1)
+elif boulder_config != None and args.init:
+    console.error("Boulder is already initialized!", doexit=True)
+elif boulder_config == None and args.init:
+    pass
+else:
+    console.log(boulder_config)
--- src/custom_functions.py
+++ src/custom_functions.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from subprocess import run
 from sys import executable as pyexe
+from types import SimpleNamespace as Namespace
+
 
 def require(module, module_name=""):
     try:
@@ -21,25 +23,45 @@ import ujson
 
 from colorama import Fore, Back, Style
 
+
 class Console:
     @staticmethod
     def log(message):
         print(f"{Fore.BLACK}{Back.CYAN} INFO {Fore.RESET}{Back.RESET}\t {message}")
+
     @staticmethod
-    def error(message):
+    def error(message, doexit: bool = False):
         print(f"{Fore.BLACK}{Back.RED} ERROR {Fore.RESET}{Back.RESET}\t {message}")
+        if doexit:
+            exit(1)
+
     @staticmethod
     def warn(message):
         print(f"{Fore.BLACK}{Back.YELLOW} WARN {Fore.RESET}{Back.RESET}\t {message}")
+
     @staticmethod
     def watch(message):
         print(f"{Fore.BLACK}{Back.BLUE} WATCH {Fore.RESET}{Back.RESET}\t {message}")
+
     @staticmethod
     def dev(message):
-        print(f"{Fore.BLACK}{Back.LIGHTBLACK_EX} DEV {Fore.RESET}{Back.RESET}\t {message}")
+        print(
+            f"{Fore.BLACK}{Back.LIGHTBLACK_EX} DEV {Fore.RESET}{Back.RESET}\t {message}"
+        )
+
     @staticmethod
     def config(message):
         print(f"{Fore.BLACK}{Back.MAGENTA} CONFIG {Fore.RESET}{Back.RESET} {message}")
+
+    @staticmethod
+    def tip(message):
+        print(
+            f"{Fore.BLACK}{Back.LIGHTYELLOW_EX} TIP {Fore.RESET}{Back.RESET}\t {message}"
+        )
+
+
+console = Console()
+
 
 def load_json(json_path):
     with open(json_path, "r") as f:
@@ -50,8 +72,10 @@ def dump_json(json_path, data):
     with open(json_path, "w") as f:
         ujson.dump(data, f, indent=4)
 
+
 def project_path():
     return Path(os.getcwd())
+
 
 def load_boulder_config():
     current_path = project_path()
@@ -60,26 +84,48 @@ def load_boulder_config():
         if config_path.exists():
             return load_json(config_path)
         current_path = current_path.parent
-    Console.error("Could not find boulder_config.json")
+
 
 def boulder_path():
     return Path(__file__).parent
+
+
 def load_global_config():
     global_location = boulder_path() / "global_config.json"
     if global_location.exists():
         return load_json(global_location)
 
+
 class Git:
-    def __init__(self, repo_url:str, local_path:str):
+    def __init__(self, repo_url: str, local_path: str):
         self.repo_url = repo_url
         self.local_path = local_path
+
     def clone(self):
         if not os.path.exists(self.local_path):
-            result = run(["git", "clone", self.repo_url, self.local_path], capture_output=True, text=True)
-            return result.stdout + result.stderr
-        return "Repo already exists."
+            result = run(
+                ["git", "clone", self.repo_url, self.local_path],
+                capture_output=True,
+                text=True,
+            )
+            if result.stderr == "":
+                return Namespace(success=True, output=result.stdout)
+            else:
+                return Result(success=False, output=result.stderr)
+        else:
+            console.error(f"{self.local_path} already exists!")
+            console.tip("Did you mean to pull?")
+            exit(1)
+
     def pull(self):
         if os.path.exists(self.local_path):
-            result = run(["git", "-C", self.local_path, "pull"], capture_output=True, text=True)
-            return result.stdout + result.stderr
-        return "Repo not found. Clone it first."
+            result = run(
+                ["git", "-C", self.local_path, "pull"], capture_output=True, text=True
+            )
+            if result.stderr == "":
+                return Result(success=True, output=result.stdout)
+            else:
+                return Result(success=False, output=result.stderr)
+        else:
+            console.error(f"{self.local_path} isn't a valid repo!")
+            console.tip("Did you mean to clone?")
