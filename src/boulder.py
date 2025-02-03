f207c92c5738cb853adc921b65a873dd41eee018
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-02-03 12:57:46 +0800

    almost done with the builder
    i literally havent tested it lol

--- src/boulder.py
+++ src/boulder.py
@@ -1,7 +1,10 @@
 from custom_functions import *
+from shutil import copytree, rmtree
+from subprocess import run
+from maps import *
 
 try:
-    from os import makedirs
+    from os import makedirs, chdir
 
     require("colorama")
     from colorama import *
@@ -18,6 +21,7 @@ try:
     console.tips = global_config["show_tips"]
     console.log("Loaded global config", vb=True)
     console.log("Loaded arguments", vb=True)
+    console.log(args, vb=True)
     boulder_config = load_boulder_config()
     if boulder_config == None:
         console.warn("Project config not found", vb=True)
@@ -47,6 +51,64 @@ try:
         dump_json("boulder_config.json", template_config)
         if global_config["show_tips"]:
             console.tip("Hooks can be set up to make your experience a lot better!")
-    else: pass
+    else:
+        if args.build:
+            project_loc = project_path()
+            main_loc = boulder_path()
+            console.log("Building started")
+            try:
+                makedirs(f"{main_loc}/build/{boulder_config['manifest']['name']}")
+            except FileExistsError:
+                console.warn("Build directory already exists", vb=True)
+            rmtree(f"{main_loc}/build/{boulder_config['manifest']['name']}")
+            copytree(f"{project_loc}/src", f"{main_loc}/build/{boulder_config['manifest']['name']}")
+            chdir(f"{main_loc}/build/{boulder_config['manifest']['name']}")
+            console.log("Copied files to seperate directory", vb=True)
+            console.log("Loading hooks")
+            try:
+                makedirs(f"{main_loc}/hooks")
+            except FileExistsError:
+                pass
+            try:
+                for remote_hook in boulder_config["hooks"]["remote"]:
+                    repo = Git(f"{main_loc}/hooks", remote_hook["repo"])
+                    output = repo.clone()
+                    if output.output == None:
+                        # Repo already exists
+                        repo.pull()
+                        if not output.success:
+                            console.error("Could not clone hooks!")
+                            raise BuildIssue(output.output)
+                    elif not output.success:
+                        console.error("Could not clone hooks!")
+                        raise BuildIssue(output.output)
+                    else:
+                        try:
+                            if remote_hook["checkout_type"] == "branch" and remote_hook["checkout"] != repo.branch:
+                                repo.checkout("branch", remote_hook["checkout"])
+                        except KeyError:
+                            pass
+                        
+                    try:
+                        if remote_hook["regolith"]:
+                            # Regolith has a different format
+                            filter_def = load_json(f"{repo.local_path}/{remote_hook["name"]}/filter.json")
+                            run([filter_def["filters"][0]["runWith"], f"{repo.local_path}/{remote_hook["name"]}/{filter_def["filters"][0]["script"]}"])
+                        else:
+                            raise KeyError # why is regolith false, what are you doing
+                    except KeyError:
+                        boulder_hooks = load_json(f"{repo.local_path}/.boulder_hooks.json")
+                        for hook in boulder_hooks:
+                            if hook["name"] == remote_hook["name"]:
+                                for commands in hook["run"]:
+                                    run(shell_caller(f"{repo.local_path}/{commands}"))
+                for local_hook in boulder_config["hooks"]["local"]:
+                    run(shell_caller(f"{project_loc}/{local_hook["file"]}"))
+            except KeyError:
+                console.error("Hook format is invalid!")
+                console.tip("Try making a hook with boulder instead of manually!")
+                exit(1)
+            except BuildIssue as e:
+                console.error(e, doexit=True)
 except KeyboardInterrupt:
     console.error("KeyboardInterrupt", doexit=True)
--- src/custom_functions.py
+++ src/custom_functions.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 from pathlib import Path
 from subprocess import run
 from sys import executable as pyexe
@@ -29,51 +29,62 @@ import ujson
 
 
 class Console:
-    def __init__(self, verbose:bool=False, allow_tips:bool=True):
+    def __init__(self, verbose: bool = False, allow_tips: bool = True):
         self.verbose = verbose
         self.tips = allow_tips
 
-    def log(self, message, vb:bool=False):
-        if (vb and self.verbose) or not (vb or self.verbose):
-            print(f"{Fore.BLACK}{Back.CYAN} INFO {Fore.RESET}{Back.RESET}\t {message}")
+    def _format(self, tag, color, message):
+        tag = tag.ljust(6)  # Centers the text inside the tag
+        print(f"{Fore.BLACK}{color} {tag} {Fore.RESET}{Back.RESET} {message}")
 
-    def error(self, message, doexit: bool = False, vb:bool=False):
-        if (vb and self.verbose) or not (vb or self.verbose):
-            print(f"{Fore.BLACK}{Back.RED} ERROR {Fore.RESET}{Back.RESET}\t {message}")
+    def log(self, message, vb: bool = False):
+        if (vb and self.verbose) or not vb:
+            self._format("INFO", Back.CYAN, message)
+
+    def error(self, message, doexit: bool = False, vb: bool = False):
+        if (vb and self.verbose) or not vb:
+            self._format("ERROR", Back.RED, message)
             if doexit and not (vb and self.verbose):
                 exit(1)
 
-    def warn(self, message, vb:bool=False):
-        if (vb and self.verbose) or not (vb or self.verbose):
-            print(f"{Fore.BLACK}{Back.YELLOW} WARN {Fore.RESET}{Back.RESET}\t {message}")
+    def warn(self, message, vb: bool = False):
+        if (vb and self.verbose) or not vb:
+            self._format("WARN", Back.YELLOW, message)
 
-    def watch(self, message, vb:bool=False):
-        if (vb and self.verbose) or not (vb or self.verbose):
-            print(f"{Fore.BLACK}{Back.BLUE} WATCH {Fore.RESET}{Back.RESET}\t {message}")
+    def watch(self, message, vb: bool = False):
+        if (vb and self.verbose) or not vb:
+            self._format("WATCH", Back.BLUE, message)
 
-    def dev(self, message, vb:bool=False):
-        if (vb and self.verbose) or not (vb or self.verbose):
-            print(
-            f"{Fore.BLACK}{Back.LIGHTBLACK_EX} DEV {Fore.RESET}{Back.RESET}\t {message}"
+    def dev(self, message, vb: bool = False):
+        if (vb and self.verbose) or not vb:
+            self._format("DEV", Back.LIGHTBLACK_EX, message)
+
+    def config(self, message, vb: bool = False):
+        if (vb and self.verbose) or not vb:
+            self._format("CONFIG", Back.MAGENTA, message)
+
+    def tip(self, message):
+        if self.tips:
+            self._format("TIP", Back.LIGHTYELLOW_EX, message)
+
+    def input(self, message):
+        return input(
+            f"{Fore.BLACK}{Back.LIGHTWHITE_EX} INPUT  {Fore.RESET}{Back.RESET} {message} "
         )
 
-    def config(self, message, vb:bool=False):
-        if (vb and self.verbose) or not (vb or self.verbose):
-            print(f"{Fore.BLACK}{Back.MAGENTA} CONFIG {Fore.RESET}{Back.RESET} {message}")
-
-    def tip(self, message, vb:bool=False):
-        if ((vb and self.verbose) or not (vb or self.verbose)) and self.tips:
-            print(
-                f"{Fore.BLACK}{Back.LIGHTYELLOW_EX} TIP {Fore.RESET}{Back.RESET}\t {message}"
-            )
-
-    def input(self, message, vb:bool=False):
-        if (vb and self.verbose) or not (vb or self.verbose):
-            return input(
-                f"{Fore.BLACK}{Back.LIGHTWHITE_EX} INPUT {Fore.RESET}{Back.RESET}\t {message} "
-            )
 
 console = Console()
+
+"""
+console.log("Log")
+console.warn("Warn")
+console.watch("Watch")
+console.dev("Dev")
+console.config("Config")
+console.tip("Tip")
+console.input("Input")
+"""
+
 try:
     require("requests")
     import requests
@@ -132,15 +143,69 @@ def load_from_remote(pathFromBaseRepo: str, isJson: bool = False):
         exit(1)
 
 
+class BuildIssue(Exception):
+    pass
+
+def check_branch(result):
+    result = result.stdout.split("\n")[0][2:]
+    version_match = re.search(r"\(HEAD detached at (v[^)]+)\)", result)
+    commit_match = re.search(r"\(HEAD detached at ([^)]+)\)", result)
+    if version_match:
+        return version_match.group(1)
+    elif commit_match:
+        return commit_match.group(1)
+    else:
+        return result
+
 class Git:
     def __init__(self, repo_url: str, local_path: str):
         self.repo_url = repo_url
         self.local_path = local_path
+        if os.path.exists(self.local_path):
+            self.branch = check_branch(run(["git", "-C", self.local_path, "branch"], capture_output=True, text=True))
+            
+                
 
     def clone(self):
         if not os.path.exists(self.local_path):
             result = run(
-                ["git", "clone", self.repo_url, self.local_path],
+                ["git", "clone", self.repo_url, self.local_path, "--depth=1"],
+                capture_output=True,
+                text=True,
+            )
+            if result.stderr == "":
+                self.branch = check_branch(run(["git", "-C", self.local_path, "branch"], capture_output=True, text=True))
+                return Namespace(success=True, output=result.stdout)
+            else:
+                return Namespace(success=False, output=result.stderr)
+        else:
+            return Namespace(success=False, output=None)
+
+    def pull(self):
+        if os.path.exists(self.local_path):
+            os.chdir(self.local_path)
+            # I love black's formatting
+            result = run(
+                [
+                    "git",
+                    "fetch",
+                    "--depth=1",
+                    "&&",
+                    "git",
+                    "reset",
+                    "--hard",
+                    "origin/main",
+                    "&&",
+                    "git",
+                    "reflog",
+                    "expire",
+                    "--all",
+                    "--expire=now",
+                    "&&",
+                    "git",
+                    "gc",
+                    "--prune=now",
+                ],
                 capture_output=True,
                 text=True,
             )
@@ -149,22 +214,24 @@ class Git:
             else:
                 return Namespace(success=False, output=result.stderr)
         else:
-            console.error(f"{self.local_path} already exists!")
-            console.tip("Did you mean to pull?")
-            exit(1)
+            return Namespace(success=False, output=None)
 
-    def pull(self):
+    def checkout(self, type:str, to_what: str):
         if os.path.exists(self.local_path):
+            if type == "tag":
+                to_what = f"tags/{to_what}"
             result = run(
-                ["git", "-C", self.local_path, "pull"], capture_output=True, text=True
-            )
+                ["git", "-C", self.local_path, "checkout", to_what],
+                capture_output=True,
+                text=True,
+            )    
             if result.stderr == "":
+                self.branch = check_branch(run(["git", "-C", self.local_path, "branch"], capture_output=True, text=True))
                 return Namespace(success=True, output=result.stdout)
             else:
                 return Namespace(success=False, output=result.stderr)
         else:
-            console.error(f"{self.local_path} isn't a valid repo!")
-            console.tip("Did you mean to clone?")
+            return Namespace(success=False, output=None)
 
 
 def parser(args):
@@ -172,59 +239,95 @@ def parser(args):
         f"Boulder: {Fore.GREEN}Regolith, built with Python{Fore.RESET}",
         {
             "arg": "build",
-            "help": f"Builds the project to your other folder. {Fore.CYAN}This is automatically enabled when running --watch and/or --dev{Fore.RESET}",
+            "help": f"Builds the project to your other folder.",
             "action": "store_true",
+            "conflict": ["run-hooks", "add-hooks"],
         },
         {
             "arg": "dev",
             "help": "Builds and moves the project automatically to the development folder",
             "action": "store_true",
+            "enable": ["build"],
+            "conflict": ["run-hooks", "add-hooks"],
         },
-        {"arg": "init", "help": "Initialize a Boulder project", "action": "store_true"},
+        {
+            "arg": "init",
+            "help": "Initialize a Boulder project",
+            "action": "store_true",
+            "conflict": ["run-hooks", "add-hooks"],
+        },
         {
             "arg": "watch",
             "help": "Watches the project for changes and builds it automatically",
             "action": "store_true",
+            "enable": ["build"],
+            "conflict": ["run-hooks", "add-hooks"],
         },
         {"arg": "verbose", "help": "Enable verbose output", "action": "store_true"},
         {
             "arg": "run-hooks",
-            "help": 'Run hooks provided comma separated ("prebuild,postbuild")',
+            "help": f'Run hooks provided comma separated ({Fore.CYAN}"prebuild,postbuild"{Fore.RESET})',
             "action": "store_list",
-            "seperate_by": ",",
+            "separate_by": ",",
+            "conflict": ["build", "dev", "watch", "init", "add-hooks"],
+        },
+        {
+            "arg": "add-hooks",
+            "help": "Add hooks to the project",
+            "action": "store_true",
+            "conflict": ["run-hooks", "build", "dev", "watch", "init"],
         },
     ]
     parsed_args = Namespace()
+    enabled_args = set()
+
     if "help" in args or args == []:
         toprint = ""
         usage = "usage: boulder"
         for arg in arguments[1:]:
-            toprint += f"\n  {arg['arg']}{(12 - len(arg['arg'])) * " "}{arg['help']}"
+            toprint += f"\n  {arg['arg']}{(12 - len(arg['arg'])) * ' '}{arg['help']}"
             if arg["action"] != "store_true":
                 usage += f" [{arg['arg']} ...]"
             usage += f" [{arg['arg']}]"
-        print(usage)
-        print()
-        print(arguments[0])
-        print(f"\nArguments:")
-        print(f"{toprint[1:]}\n  help{" " * 8}Show this message")
+        print(
+            f"{usage}\n\n{arguments[0]}\n\nArguments:\n{toprint[1:]}\n  help{' ' * 8}Show this message"
+        )
         exit(0)
     else:
         for arg in arguments[1:]:
             if arg["arg"] in args:
                 if arg["action"] == "store_true":
                     setattr(parsed_args, arg["arg"], True)
+                    enabled_args.add(arg["arg"])
                 elif arg["action"] == "store_list":
                     setattr(
                         parsed_args,
                         arg["arg"],
-                        args[args.index(arg["arg"]) + 1].split(arg["seperate_by"]),
+                        args[args.index(arg["arg"]) + 1].split(arg["separate_by"]),
                     )
+                    enabled_args.add(arg["arg"])
                 else:
                     setattr(parsed_args, arg["arg"], args[args.index(arg["arg"]) + 1])
+                    enabled_args.add(arg["arg"])
             else:
                 if arg["action"] == "store_true":
                     setattr(parsed_args, arg["arg"], False)
                 else:
                     setattr(parsed_args, arg["arg"], None)
+        # Enable if enabled
+        for arg in arguments[1:]:
+            if arg["arg"] in enabled_args and "enable" in arg:
+                for enable_arg in arg["enable"]:
+                    setattr(parsed_args, enable_arg, True)
+                    enabled_args.add(enable_arg)
+        # Check if conflict
+        for arg in arguments[1:]:
+            if arg["arg"] in enabled_args and "conflict" in arg:
+                for conflict_arg in arg["conflict"]:
+                    if conflict_arg in enabled_args:
+                        console.error(
+                            f"Argument '{Fore.GREEN}{arg['arg']}{Fore.RESET}' conflicts with '{Fore.RED}{conflict_arg}{Fore.RESET}'",
+                            doexit=True,
+                        )
+
         return parsed_args
